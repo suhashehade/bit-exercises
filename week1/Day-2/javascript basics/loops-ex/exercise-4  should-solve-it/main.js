8bf0b673bfdf7cf1db6f3861f912e20c94cd38bb
suha <suhashehahde@gmail.com>
2022-07-04 10:57:16 -0700

    add loops-ex

---
+++ week1/Day-2/javascript basics/loops-ex/exercise-4  should-solve-it/main.js
@@ -0,0 +1,23 @@
+/*should solve it*/ 
+
+const posts = [
+    {
+      id: 1, 
+      text: "Love this product",
+      comments: []
+    },
+    { 
+      id: 2, 
+      text: "This is the worst. DON'T BUY!", 
+      comments: [
+                  {id: 1, text: "Idiot has no idea"}, 
+                  {id: 2, text:"Fool!"}, 
+                  {id: 3, text: "I agree!"}
+                ]
+     },
+     {
+      id: 3, 
+      text: "So glad I found this. Bought four already!",
+      comments: []
+     }
+  ]

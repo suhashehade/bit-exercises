208fb8088cadea45ef1be95746c4b16d3c5cd2ba
suha <suhashehahde@gmail.com>
2022-07-13 10:33:19 -0700

    add exercise-4

---
+++ week1/Day-2/javascript basics/loops-ex/exercise-4/main.js
@@ -0,0 +1,31 @@
+const posts = [
+  {
+    id: 1,
+    text: "Love this product",
+    comments: [],
+  },
+  {
+    id: 2,
+    text: "This is the worst. DON'T BUY!",
+    comments: [
+      { id: 1, text: "Idiot has no idea" },
+      { id: 2, text: "Fool!" },
+      { id: 3, text: "I agree!" },
+    ],
+  },
+  {
+    id: 3,
+    text: "So glad I found this. Bought four already!",
+    comments: [],
+  },
+];
+
+for (let post of posts) {
+  if (post.id === 2) {
+    for (let comment of post.comments) {
+      if (comment.id === 3) {
+        console.log(comment.text);
+      }
+    }
+  }
+}

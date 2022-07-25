f857dc2ae75b8f045ab5865f85724f181d0c9f7f
suha <suhashehahde@gmail.com>
2022-07-24 20:07:30 -0700

    add express intro exercise

---
+++ week5/day-1/express-intro/furniture-store/dist/main.js
@@ -0,0 +1,19 @@
+
+
+const getFurniturePrice = function(){
+    let itemName = $('#search-price').val();
+    $.get(`/priceCheck/${itemName}`, function(response) {
+        $("body").append(
+           `<div>${response.price}</div>`
+        )
+   })
+}
+
+const reduceInventory = function(){
+    let itemName = $('#reduce-inventory').val();
+    $.get(`/buy/${itemName}`, function(response){
+        $("body").append(
+            `<p>Congratulations, you’ve just bought ${itemName} for ${response.price}. There are ${response.inventory} left now in the store.</p>`
+         )
+    })
+}

a48b04af64a101b9cafb5c2bc7ef5b7b8bb9484e
suha <suhashehahde@gmail.com>
2022-08-15 16:22:16 -0700

    add react intro exercises

---
+++ week8/day-2/react -intro/lesson/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    let text = 'dynamically'
+    return (
+      <h1>Going to display some text...{text}</h1>
+    );
+  }
+}
+
+export default App;
